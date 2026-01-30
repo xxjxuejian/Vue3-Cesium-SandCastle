ad25e004505da58c6b0a66c3283b721851d4de8c
xu xuejian <2498492824@qq.com>
2026-01-30 16:19:47 +0800

    ele-pluse组件库的国际化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import en from "element-plus/es/locale/lang/en";
+import { useAppStore } from "@/stores/modules/app.store";
+const appStore = useAppStore();
+</script>
 
 <template>
-  <router-view />
+  <el-config-provider :locale="appStore.language === 'zh-cn' ? zhCn : en">
+    <router-view></router-view>
+  </el-config-provider>
 
   <!-- unocss 图标 -->
   <!-- <div class="flex-center bg-red-100 p-4">
