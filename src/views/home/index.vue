0d6e7c8e831252500a3ebf61542e7a77df04aef6
xu xuejian <2498492824@qq.com>
2026-02-04 17:21:08 +0800

    首页框架设计

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Search } from "@element-plus/icons-vue";
+const searchValue = ref("");
+const selectValue = ref("");
+const options = [];
+</script>
 
 <template>
-  <main>homeView</main>
+  <div class="p-4">
+    <!-- 按标签搜素 -->
+    <div class="flex items-center gap-x-4 max-w-lg">
+      <span class="font-700 text-xl">Gallery</span>
+      <el-input v-model="searchValue" placeholder="Search gallery" :prefix-icon="Search" />
+      <el-select v-model="selectValue" placeholder="Select" style="width: 240px">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+    </div>
+
+    <!-- 主内容 -->
+    <div></div>
+  </div>
 </template>
