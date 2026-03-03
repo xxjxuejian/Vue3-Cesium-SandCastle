61703ee2b0b3a683fe43c553c9e06dca1e1a767f
xxjxuejian <2498492824@qq.com>
2026-03-03 21:42:23 +0800

    feat:首页关键字搜索

--- src/views/Home/components/Card.vue
+++ src/views/Home/components/Card.vue
@@ -20,7 +20,7 @@ const imgBaseUrl = "https://sandcastle.cesium.com/";
     class="rounded-lg bg-white shadow-sm overflow-hidden cursor-pointer transition-transform duration-300 ease-out hover:scale-105 hover:shadow-lg"
     @click="handleClick"
   >
-    <div class="py-2 px-1 text-center text-sm font-medium text-gray-700 truncate">
+    <div class="py-2 px-2 text-center text-sm font-medium text-gray-700 truncate">
       {{ translateRouteTitle(caseInfo.title) }}
     </div>
     <div class="w-full h-[150px] bg-gray-100">
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,47 +3,64 @@ import { transformGalleryDataToMap, generateHomeListData } from "@/utils/transfo
 import type { galleryData } from "@/types/api/home";
 import { translateRouteTitle } from "@/lang/utils";
 import Card from "./components/Card.vue";
+
+interface SelectOption {
+  label: string;
+  value: string | number;
+}
+
 async function getSandCastleList() {
   const res = await fetch(import.meta.env.BASE_URL + "public/mock/galleryList.json");
   const data = await res.json();
   const entries = data.entries;
   let map = transformGalleryDataToMap(entries);
   let r = generateHomeListData(map);
-  homeList.value = r;
+  const keys = Object.keys(r);
+  // 生成select的options
+  options.value = keys.map((key) => {
+    return {
+      label: key,
+      value: key,
+    };
+  });
+  // 保存源数据和 主页展示的数据
+  originData = r;
+  homeData.value = r;
   console.log(r);
 }
 
 import { Search } from "@element-plus/icons-vue";
-const searchValue = ref("");
-const selectValue = ref("");
-const homeList = shallowRef<Record<string, galleryData[]>>();
+const searchValue = ref(""); // 搜索框的输入值
+const selectValue = ref(""); // select选择器的选中值
+let originData: Record<string, galleryData[]>; // 主页原始数据
+const homeData = shallowRef<Record<string, galleryData[]>>(); //展示的数据
+const options = ref<SelectOption[]>([]); // 数据的labels选项
 
-const options = [
-  {
-    value: "1",
-    label: "Option 1",
-  },
-  {
-    value: "2",
-    label: "Option 2",
-  },
-  {
-    value: "3",
-    label: "Option 3",
-  },
-  {
-    value: "4",
-    label: "Option 4",
-  },
-  {
-    value: "5",
-    label: "Option 5",
-  },
-  {
-    value: "6",
-    label: "Option 6",
-  },
-];
+// 按照title的值进行匹配
+const handleSearch = () => {
+  // 获取输入的搜索内容
+  const searchContent = searchValue.value;
+  if (!searchContent) homeData.value = originData;
+  const tempObj: any = {};
+  for (const key in originData) {
+    const res = originData[key].filter((item) => {
+      return item.title.includes(searchContent);
+    });
+    if (res.length > 0) {
+      tempObj[key] = res;
+    }
+  }
+  homeData.value = tempObj;
+};
+// 监听select选择器的变化
+const handleLabelChange = (value: string) => {
+  //确保value对应的key,在originData中存在
+  if (value in originData) {
+    homeData.value = { [value]: originData[value] };
+  } else {
+    homeData.value = originData;
+  }
+};
 
 onMounted(() => {
   getSandCastleList();
@@ -53,10 +70,25 @@ onMounted(() => {
 <template>
   <div class="p-4 wh-full overflow-auto">
     <!-- 按标签搜索-->
-    <div class="flex items-center gap-x-4 max-w-lg">
+    <div class="flex items-center gap-x-4">
       <span class="font-700 text-xl">Gallery</span>
-      <el-input v-model="searchValue" placeholder="Search gallery" :prefix-icon="Search" />
-      <el-select v-model="selectValue" placeholder="Select" style="width: 240px">
+      <el-input
+        v-model="searchValue"
+        placeholder="Search gallery"
+        clearable
+        :prefix-icon="Search"
+        style="width: 240px"
+      />
+
+      <el-button type="primary" @click="handleSearch">Search</el-button>
+
+      <el-select
+        v-model="selectValue"
+        placeholder="Labels"
+        @change="handleLabelChange"
+        style="width: 100px"
+      >
+        <el-option label="All" value="all" />
         <el-option
           v-for="item in options"
           :key="item.value"
@@ -67,17 +99,16 @@ onMounted(() => {
     </div>
 
     <!-- 主内容 -->
-    <!-- <div class="parent">
-      <el-input v-model="input" style="width: 240px" placeholder="Please input" />
-    </div> -->
-
     <div>
-      <section v-for="(value, key) in homeList" :key="key" class="mb-4">
+      <section v-for="(value, key) in homeData" :key="key" class="mb-4">
         <h2 class="mb-4 text-lg font-semibold text-gray-800">
           {{ translateRouteTitle(key) }}
         </h2>
-        <div class="grid gap-6 grid-cols-[repeat(auto-fit,minmax(225px,1fr))]">
-          <Card v-for="item in value" :key="item.id" :case-info="item" />
+        <!-- class="grid gap-6 grid-cols-[repeat(auto-fit,minmax(225px,250px))] bg-red-100" -->
+        <div>
+          <div class="w-243 mx-auto bg-green-100 grid gap-6 grid-cols-[repeat(auto-fit,225px)]">
+            <Card v-for="item in value" :key="item.id" :case-info="item" />
+          </div>
         </div>
       </section>
     </div>
