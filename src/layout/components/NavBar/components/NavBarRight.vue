9ba83cdf5fd5c2d8bb0addeb2bd067c3c20aa317
xu xuejian <2498492824@qq.com>
2026-04-01 15:47:47 +0800

    feat:add github link

--- src/layout/components/NavBar/components/NavBarRight.vue
+++ src/layout/components/NavBar/components/NavBarRight.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import FullScreen from "@/components/FullScreen/index.vue";
 import LangSelect from "@/components/LangSelect/index.vue";
+
+const handleGithub = () => {
+  window.open("https://github.com/xxjxuejian/Vue3-Cesium-SandCastle", "_blank");
+};
 </script>
 
 <template>
@@ -11,6 +15,10 @@ import LangSelect from "@/components/LangSelect/index.vue";
 
     <div class="navbar-right-item min-w-[44px] h-full flex-center cursor-pointer hover:bg-gray-200">
       <LangSelect size="text-lg" />
+    </div>
+
+    <div class="navbar-right-item min-w-[44px] h-full flex-center cursor-pointer hover:bg-gray-200">
+      <div class="i-svg:github text-xl" @click="handleGithub" />
     </div>
   </div>
 </template>
